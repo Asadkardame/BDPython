6c3a8e11cd423e0d5d6df71ccc3020a28f2e2c6b
Asad Kardame <asadkardame@gmaill.com>
2024-02-09 00:13:17 +0000

    Keyword argument, 4.8.2

---
+++ Python/Keyword_Argument.py
@@ -0,0 +1,44 @@
+def parrot(voltage, state='a stiff', action='voom', type='Norwegian Blue'):
+    print("-- This parrot wouldn't", action, end=' ')
+    print("if you put", voltage, "volts through it.")
+    print("-- Lovely plumage, the", type)
+    print("-- It's", state, "!")
+
+
+# accepts one required argument (voltage) and three optional arguments (state, action, and type). 
+    # This function can be called in any of the following ways:
+
+# parrot(1000)                                          # 1 positional argument
+# parrot(voltage=1000)                                  # 1 keyword argument
+# parrot(voltage=1000000, action='VOOOOOM')             # 2 keyword arguments
+# parrot(action='VOOOOOM', voltage=1000000)             # 2 keyword arguments
+parrot('a million', 'bereft of life', 'jump')         # 3 positional arguments
+# parrot('a thousand', state='pushing up the daisies')  # 1 positional, 1 keyword
+
+# but all the following calls would be invalid:
+
+
+# parrot()                     # required argument missing
+# parrot(voltage=5.0, 'dead')  # non-keyword argument after a keyword argument
+# parrot(110, voltage=220)     # duplicate value for the same argument
+# parrot(actor='John Cleese')  # unknown keyword argument
+    
+#------------------------------------------------------------------------------------------------
+    
+
+
+def cheeseshop(kind, *arguments, **keywords):
+    print("-- Do you have any", kind, "?")
+    print("-- I'm sorry, we're all out of", kind)
+    for arg in arguments:
+        print(arg)
+    print("-" * 40)
+    for kw in keywords:
+        print(kw, ":", keywords[kw])
+
+
+cheeseshop("Limburger", "It's very runny, sir.",
+           "It's really very, VERY runny, sir.",
+           shopkeeper="Michael Palin",
+           client="John Cleese",
+           sketch="Cheese Shop Sketch")
